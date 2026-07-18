c9b07b6fa2f27ec1aa67529d28b45933004eb27a
Winkarst-cpu <74284083+Winkarst-cpu@users.noreply.github.com>
2026-07-18 13:59:09 +0300

    Fix

--- Content.Shared/DoAfter/SharedDoAfterSystem.cs
+++ Content.Shared/DoAfter/SharedDoAfterSystem.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Bed.Sleep;
-using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
 using Content.Shared.Hands.Components;
 using Content.Shared.Interaction;
 using Content.Shared.Movement.Events;
